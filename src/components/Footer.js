ecb14367697a3606e9af43948833d1237fea469a
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-14 10:50:44 +0200

    Create component with simple basic content

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  
+  render() {
+    return (
+      <React.Fragment>
+        Footer
+      </React.Fragment>
+    );
+  }
+
+}
