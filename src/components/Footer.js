87daee536409ce37a1c337a533e9fd414e6cdaca
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-14 15:42:58 +0200

    Add footer navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Footer extends Component {
-  
   render() {
     return (
-      <React.Fragment>
-        Footer
-      </React.Fragment>
+      <footer>
+        <div>
+          <p>&copy; <a href="https://peter-stuhlmann-webentwicklung.de/">Peter R. Stuhlmann web development</a>, 2019</p>
+          <nav>
+            <ul>
+              <li><Link to="/">CountryCard</Link></li>
+              <li><Link to="/legalnotice">Legal notice</Link></li>
+              <li><Link to="/privacypolicy">Privacy policy</Link></li>
+              <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/peter-stuhlmann/">Github</a></li>
+            </ul>
+          </nav>
+        </div>
+      </footer>
     );
   }
-
 }
