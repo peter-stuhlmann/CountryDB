49e43a420ab89de5a887bcb5e8cfb61bd55bc02d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-14 12:12:49 +0200

    Import and use class Country

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Country from './Country';
 
 export default class Main extends Component {
   
   render() {
     return (
       <React.Fragment>
-        Main
+        <Country />
       </React.Fragment>
     );
   }
